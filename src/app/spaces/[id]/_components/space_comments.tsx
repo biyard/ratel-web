fb36a61437251c1f2c831390867c6b2dca129811
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-06-09 20:31:42 +0900

    add comment

--- src/app/spaces/[id]/_components/space_comments.tsx
+++ src/app/spaces/[id]/_components/space_comments.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import Comment from '@/assets/icons/comment.svg';
 import { SpaceComment } from '@/lib/api/models/comments';
