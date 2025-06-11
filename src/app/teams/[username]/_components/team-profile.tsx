7b95450356a08f74cf348b083b28afed75635a36
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-06-11 10:05:36 +0900

    fix layout

--- src/app/teams/[username]/_components/team-profile.tsx
+++ src/app/teams/[username]/_components/team-profile.tsx
@@ -4,17 +4,21 @@ import { Team } from '@/lib/api/models/team';
 import TeamSelector from '@/app/(social)/_components/team-selector';
 
 export interface TeamProfileProps {
-  team: Team;
+  team?: Team;
 }
 
 export default function TeamProfile({ team }: TeamProfileProps) {
+  if (!team) {
+    return <div></div>;
+  }
+
   return (
     <div className="flex flex-col gap-5 px-4 py-5 rounded-[10px] bg-component-bg">
       <TeamSelector team={team} />
       <div className="relative">
         <Image
-          src={team.profile_url || '/default-profile.png'}
-          alt={team.nickname}
+          src={team?.profile_url || '/default-profile.png'}
+          alt={team?.nickname ?? 'team profile'}
           width={80}
           height={80}
           className="rounded-full border-2 object-cover object-top w-[80px] h-[80px]"
--- src/app/teams/layout.tsx
+++ src/app/teams/layout.tsx
@@ -4,13 +4,22 @@ import Loading from '@/app/loading';
 import { useUserInfo } from '@/lib/api/hooks/users';
 import { UserType } from '@/lib/api/models/user';
 import UserSidemenu from '../(social)/_components/user-sidemenu';
+import { usePathname } from 'next/navigation';
 
 export default function TeamLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const isTeamPage = /^\/teams\/[^/]+$/.test(pathname);
+
   const { data: user, isLoading } = useUserInfo();
+
+  if (isTeamPage) {
+    return <>{children}</>;
+  }
+
   return (
     <div className="flex min-h-screen gap-5 justify-between max-w-6xl mx-auto text-white py-3 px-2.5">
       {!isLoading &&
