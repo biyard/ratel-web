05d08d3afe0785da294fbc82843d86347876884c
ryancho-0 <ryan@biyard.co>
2025-06-18 11:16:15 +0900

    chore: lint

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 //FIXME: This logic needs to be implemented on the backend. And just call the logout endpoint like proxy.
-export async function POST(request: NextRequest) {
+export async function POST() {
   const response = NextResponse.json(
     { message: 'Logout successful' },
     { status: 200 },
