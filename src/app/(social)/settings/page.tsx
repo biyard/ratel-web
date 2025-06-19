c66f46f8b0a22fe48323aae5fb201cbb714a1463
hackartists <hackartists@gmail.com>
2025-06-19 12:27:10 +0900

    fix wallet connect in setting page

--- src/app/(social)/settings/page.tsx
+++ src/app/(social)/settings/page.tsx
@@ -17,6 +17,7 @@ import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import WalletSummary from './_components/wallet-summary';
 import Image from 'next/image';
+import { logger } from '@/lib/logger';
 
 export default function MyProfilePage() {
   const { post } = useApiCall();
@@ -27,6 +28,7 @@ export default function MyProfilePage() {
   const [profileUrl, setProfileUrl] = useState(user?.profile_url || '');
   const [nickname, setNickname] = useState(user?.nickname);
   const [htmlContents, setHtmlContents] = useState(user?.html_contents);
+  const [showWalletConnect, setShowWalletConnect] = useState(false);
 
   const handleContents = (evt: React.FormEvent<HTMLTextAreaElement>) => {
     setHtmlContents(evt.currentTarget.value);
@@ -78,6 +80,39 @@ export default function MyProfilePage() {
           <Input type="text" disabled value={`@${user?.username}`} />
         </Row>
         <Row className="max-tablet:flex-col">
+          <label className="w-40 font-bold">EVM Address</label>
+          <Row>
+            <Input type="text" disabled value={`${user?.evm_address}`} />
+            <Button
+              variant={'rounded_secondary'}
+              className="py-0 rounded-sm"
+              onClick={() => setShowWalletConnect(!showWalletConnect)}
+            >
+              {showWalletConnect ? 'Hide' : 'Change'}
+            </Button>
+          </Row>
+        </Row>
+        <Row
+          className="w-full aria-hidden:hidden"
+          aria-hidden={!showWalletConnect}
+        >
+          <WalletSummary
+            onUpdate={async (address) => {
+              logger.debug('Updating wallet address...', address);
+
+              await post(ratelApi.users.updateEvmAddress(), {
+                update_evm_address: {
+                  evm_address: address,
+                },
+              });
+
+              userinfo.refetch();
+              setShowWalletConnect(false);
+            }}
+          />
+        </Row>
+
+        <Row className="max-tablet:flex-col">
           <label className="w-40 font-bold">Display name</label>
           <Input
             type="text"
@@ -107,8 +142,6 @@ export default function MyProfilePage() {
             Save
           </Button>
         </Row>
-
-        <WalletSummary />
       </Col>
     </div>
   );
