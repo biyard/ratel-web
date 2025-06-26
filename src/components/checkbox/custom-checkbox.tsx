38f135eadfbc5f8e6ebb90ef3604bcd5803536d6
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-06-27 08:55:42 +0900

    fix lint

--- src/components/checkbox/custom-checkbox.tsx
+++ src/components/checkbox/custom-checkbox.tsx
@@ -2,7 +2,7 @@ interface CustomCheckboxProps {
   isRounded?: boolean;
   checked: boolean;
   onChange: () => void;
-  disabled: boolean;
+  disabled?: boolean;
 }
 
 export default function CustomCheckbox({
